eab0eb05f714d85412d5382ded6ace94e99d9a1c
Zhukov Georgiy <zhugeo@mail.ru>
2024-09-19 00:55:32 +0300

    RIP project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Experiment of building nvc VHDL compiler for WASM
+
+Experiment has failed. It is impossible to build nvc for WSL without massive changes because NVC uses dynamic linking and wasm does not support so.
--- configure_cmake.sh
+++ configure_cmake.sh
@@ -1,5 +1,6 @@
+#/bin/bash
 rm -rf build
 mkdir build
 cd build
 # TODO сделать скрипт переносимым
-emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/home/zhugeo/build/vcpkg/scripts/buildsystems/vcpkg.cmake
+emcmake cmake .. -DCMAKE_BUILD_TYPE=Release
