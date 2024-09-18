68a0744041c5a8ed253aab7c2aa6a468d418327f
Zhukov Georgiy <zhugeo@mail.ru>
2024-09-18 22:43:54 +0300

    Some progress

--- configure_cmake.sh
+++ configure_cmake.sh
@@ -1,6 +1,5 @@
 rm -rf build
 mkdir build
 cd build
-emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DPython3_EXECUTABLE=$(which python3) \
-                 -DPython3_INCLUDE_DIR=$(python3 -c "from sysconfig import get_paths as gp; print(gp()['include'])") \
-                 -DPython3_LIBRARY=$(python3 -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
+# TODO сделать скрипт переносимым
+emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/home/zhugeo/build/vcpkg/scripts/buildsystems/vcpkg.cmake
